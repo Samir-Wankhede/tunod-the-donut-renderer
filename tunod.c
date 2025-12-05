822831babcea565760cb6406e3fa0929b92416bc
Samir Wankhede <samirsw04@gmail.com>
2025-12-06 01:41:45 +0530

    updated donut initialization with chonk logic and fixed memory leaks with added modularisation

--- tunod.c
+++ tunod.c
@@ -28,6 +28,11 @@ typedef struct {
     int **viewport;
 } Frame;
 
+typedef struct {
+    int size;
+    Frame *frames;
+} FrameList;
+
 void RotationAboutX(int x, int y, int z, int deg, double cx, double cy, double cz, int result[3]){
     double rads = (deg * M_PI) / 180.0;
     double localY = y - cy;
@@ -42,7 +47,7 @@ void RotationAboutX(int x, int y, int z, int deg, double cx, double cy, double c
 }
 
 Donut* InitialiseEmptyDonut(int ROWS, int COLS, int THIC){
-    
+
     Donut* model = (Donut*)malloc(sizeof(Donut));
     if (model == NULL) {
         perror("Failed to allocate memory for Donut struct");
@@ -68,25 +73,29 @@ Donut* InitialiseEmptyDonut(int ROWS, int COLS, int THIC){
         ROWS / 7.0,
         THIC / 7.0
     };
+
     //matrix creation
     model->donut = (int***)malloc(model->data->THIC * sizeof(int**));
     if (model->donut == NULL) { 
         perror("Failed to allocate memory for thicc pointers");
         free(model->data);
         free(model);
-        exit(EXIT_FAILURE);
+        return NULL;
     }
     for(int i=0; i<model->data->THIC; i++){
         model->donut[i] = (int**)malloc(model->data->ROWS * sizeof(int*));
         if (model->donut[i] == NULL) {
             perror("Failed to allocate memory for row pointers");
-            for (int j = 0; j < i; j++) {
-                free(model->donut[j]);
+            for (int z = 0; z < i; z++) {
+                for (int y = 0; y < model->data->ROWS; y++) {
+                        free(model->donut[z][y]);
+                    }
+                free(model->donut[z]);
             }
             free(model->donut);
             free(model->data);
             free(model);
-            exit(EXIT_FAILURE);
+            return NULL;
         }
         for(int k=0; k<model->data->ROWS; k++){
             model->donut[i][k] = (int*)calloc(model->data->COLS, sizeof(int));
@@ -101,7 +110,7 @@ Donut* InitialiseEmptyDonut(int ROWS, int COLS, int THIC){
                 free(model->donut);
                 free(model->data);
                 free(model);
-                exit(EXIT_FAILURE);
+                return NULL;
             }
         }
     }
@@ -109,81 +118,120 @@ Donut* InitialiseEmptyDonut(int ROWS, int COLS, int THIC){
 }
 
 void InitialiseDonut(Donut* model){
-    for(int i=0; i<2; i++){
-        for(int j=0; j<model->data->ROWS; j++){
-            for(int k=0; k<model->data->COLS; k++){
-                double normX1 = (k - model->data->centerX) / model->data->radiusX1;
-                double normX2 = (k - model->data->centerX) / model->data->radiusX2;
-                double normY1 = (j - model->data->centerY) / model->data->radiusY1;
-                double normY2 = (j - model->data->centerY) / model->data->radiusY2;
-                // based on ((x-h)/rx)**2 + ((y-k)/ry)**2 = 1
-                double eDist1 = pow(normX1, 2) + pow(normY1, 2);
-                double eDist2 = pow(normX2, 2) + pow(normY2, 2);
+    double maxWallThickness = 0.15; 
 
-                if(eDist2 >= (1 + (0.2 - i*0.1)) && eDist1 <= (1 - (0.2 - i*0.1))){
-                    model->donut[i][j][k] = 1;
-                }
-            }
+    for(int i=0; i<model->data->THIC; i++){
+        double zNormal;
+        if (model->data->THIC > 1) {
+            zNormal = (i - model->data->centerZ) / model->data->centerZ; 
+        } else {
+            zNormal = 0;
         }
-    }
-    for(int i=2; i<7; i++){
+
+        // Circular Cross-Section (max at z=0)
+        double chonkiness = sqrt(1.0 - (zNormal * zNormal));
+        double currentThickness = maxWallThickness * chonkiness;
+        double innerThreshold = 0.8 - currentThickness; //0.8 was ideally 1 for ellipse but had to chose lower number for chonkiness
+        double outerThreshold = 0.8 + currentThickness;
+
         for(int j=0; j<model->data->ROWS; j++){
             for(int k=0; k<model->data->COLS; k++){
                 double normX1 = (k - model->data->centerX) / model->data->radiusX1;
                 double normX2 = (k - model->data->centerX) / model->data->radiusX2;
                 double normY1 = (j - model->data->centerY) / model->data->radiusY1;
                 double normY2 = (j - model->data->centerY) / model->data->radiusY2;
-                // based on ((x-h)/rx)**2 + ((y-k)/ry)**2 = 1
+
+                // ((x-h)/rx)^2 + ((y-k)/ry)^2 = 1
                 double eDist1 = pow(normX1, 2) + pow(normY1, 2);
                 double eDist2 = pow(normX2, 2) + pow(normY2, 2);
 
-                if(eDist2 >= 1 && eDist1 <= 1){
-                    model->donut[i][j][k] = 1;
-                }
-            }
-        }
-    }
-    for(int i=7; i<model->data->THIC; i++){
-        for(int j=0; j<model->data->ROWS; j++){
-            for(int k=0; k<model->data->COLS; k++){
-                double normX1 = (k - model->data->centerX) / model->data->radiusX1;
-                double normX2 = (k - model->data->centerX) / model->data->radiusX2;
-                double normY1 = (j - model->data->centerY) / model->data->radiusY1;
-                double normY2 = (j - model->data->centerY) / model->data->radiusY2;
-                // based on ((x-h)/rx)**2 + ((y-k)/ry)**2 = 1
-                double eDist1 = pow(normX1, 2) + pow(normY1, 2);
-                double eDist2 = pow(normX2, 2) + pow(normY2, 2);
-
-                if(eDist2 >= (1 + (0.2 - abs(i - 8)*0.1)) && eDist1 <= (1 - (0.2 - abs(i - 8)*0.1))){
-                    model->donut[i][j][k] = 1;
+                if(eDist2 >= innerThreshold && eDist1 <= outerThreshold){
+                    model->donut[i][j][k] = i < model->data->THIC/2 ? 1 : 2;
                 }
             }
         }
     }
 }
 
-Frame* generateXRotationFrames(Donut* model){
-    int ROTATIONS = 360;
-    Frame* Frames = (Frame*)malloc((ROTATIONS + 1)*sizeof(Frame));
+void freeDonutModel(Donut* model){
+    for (int z = 0; z < model->data->THIC; z++) {
+        for (int y = 0; y < model->data->ROWS; y++) {
+            free(model->donut[z][y]);
+        }
+        free(model->donut[z]);
+    }
+    free(model->donut);
+    free(model->data);
+    free(model);
+}
+
+void freeFrameBuffer(FrameList* frameList, Donut* model){
+    for(int i=0; i<frameList->size; i++){
+        for(int j=0; j<model->data->ROWS; j++) free(frameList->frames[i].viewport[j]);
+        free(frameList->frames[i].viewport);
+    }
+    free(frameList->frames);
+    free(frameList);
+}
+
+Frame* generateXRotationFrames(Donut* model, int frameBufferSize){
+    int ROTATIONS = frameBufferSize;
+
+    Frame* Frames = (Frame*)malloc((ROTATIONS)*sizeof(Frame));
     if (Frames == NULL) { 
-        perror("Failed to copy struct");
-        free(model);
-        free(Frames);
-        exit(EXIT_FAILURE);
+        perror("Failed to create frame buffer");
+        return NULL;
     }
     for(int r=0; r<=ROTATIONS; r++){
         Frames[r].viewport = (int**)malloc(model->data->ROWS * sizeof(int*));
+        if (Frames[r].viewport == NULL){
+            perror("Failed to initialize viewport Row for frame");
+            for(int j=0; j<r; j++) free(Frames[j].viewport);
+            free(Frames);
+            return NULL;
+        }
         for(int j=0; j<model->data->ROWS; j++){
             Frames[r].viewport[j] = (int*)malloc(model->data->COLS * sizeof(int));
+            if (Frames[r].viewport[j] == NULL){
+                perror("Failed to initialize viewport Column for frame");
+                for(int j=0; j<=r; j++){
+                    for(int k=0; k<j; k++) free(Frames[j].viewport[k]);
+                    free(Frames[j].viewport);
+                }
+                free(Frames);
+                return NULL;
+            }
         }
-    } 
+    }
+
+    //filling first frame
     for(int j=0; j<model->data->ROWS; j++){
         for(int k=0; k<model->data->COLS; k++){
             if(model->donut[0][j][k]) Frames[0].viewport[j][k] = 1;
         }
     }
 
+    double** zBuffer = (double**)malloc(model->data->ROWS * sizeof(double*));
+    if(zBuffer == NULL){
+        perror("Failed to initialize zBuffer rows");
+        return NULL;
+    }
+    for(int j=0; j<model->data->ROWS; j++){
+        zBuffer[j] = (double*)malloc(model->data->COLS * sizeof(double));
+        if(zBuffer[j] == NULL){
+        perror("Failed to initialize zBuffer cols");
+        for(int k=0; k<j; k++) free(zBuffer[k]);
+        free(zBuffer);
+        return NULL;
+    }
+    }
+
     for(int r=1; r<=ROTATIONS; r++){
+        for(int j=0; j<model->data->ROWS; j++){
+            for(int k=0; k<model->data->COLS; k++){
+                zBuffer[j][k] = -10000.0; 
+            }
+        }
         for(int i=0; i<model->data->THIC; i++){
             for(int j=0; j<model->data->ROWS; j++){
                 for(int k=0; k<model->data->COLS; k++){
@@ -193,33 +241,31 @@ Frame* generateXRotationFrames(Donut* model){
                         int x = transformedPoint[0];
                         int y = transformedPoint[1];
                         int z = transformedPoint[2];
+
                         if( x < 0 || x >= model->data->COLS) continue;
                         if( y < 0 || y >= model->data->ROWS) continue;
                         if( z < 0 || z >= model->data->THIC) continue;
-                        Frames[r].viewport[y][x] = 1;
+                        if (z > zBuffer[y][x]) {
+                            zBuffer[y][x] = z;
+                            Frames[r].viewport[y][x] = model->donut[i][j][k];
+                        }
                     }
                 }
             }
         }
     }
+    for(int j=0; j<model->data->ROWS; j++) free(zBuffer[j]);
+    free(zBuffer);
     return Frames;
 }
 
-int main(){
-    int ROWS = 25;
-    int COLS = 50;
-    int THIC = 9;
-
-    Donut* model = InitialiseEmptyDonut(ROWS, COLS, THIC); 
-    InitialiseDonut(model);
-
-    Frame* frames = generateXRotationFrames(model);
-
+void animate(Donut* model, Frame* frames, int frameBufferSize){
     while(1){
-        for(int i=0; i<=360; i++){
+        for(int i=0; i<=frameBufferSize; i++){
             for(int j=0; j<model->data->ROWS; j++){
                 for(int k=0; k<model->data->COLS; k++){
-                    if(frames[i].viewport[j][k]) printf(".");
+                    if(frames[i].viewport[j][k] == 2) printf("\033[36m.\033[0m");
+                    else if(frames[i].viewport[j][k] == 1) printf(".");
                     else printf(" ");
                 }
                 printf("\n");
@@ -228,7 +274,33 @@ int main(){
             system("clear");
         }
     }
+}
 
-    free(model);
+int main(){
+    int ROWS = 25;
+    int COLS = 50;
+    int THIC = 9;
+
+    Donut* model = InitialiseEmptyDonut(ROWS, COLS, THIC);
+    if(model == NULL){
+        perror("Could not initialise model.");
+        return 1;
+    }
+
+    InitialiseDonut(model);
+
+    FrameList* frameList = (FrameList*)malloc(sizeof(FrameList));
+    if(frameList == NULL){
+        perror("Could not initialise frameList buffer.");
+        freeDonutModel(model);
+        return 1;
+    }
+    frameList->size = 361;
+    frameList->frames = generateXRotationFrames(model, frameList->size);
+
+    animate(model, frameList->frames, frameList->size);
+
+    freeFrameBuffer(frameList, model);
+    freeDonutModel(model);
     return 0;
 }
